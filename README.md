8f3053e2af53f2a762d040d86d1f86977b262dbf
Kors <korsvanloon@gmail.com>
2015-03-11 15:55:03 +0100

    init from bbclean

---
+++ README.md
@@ -0,0 +1,36 @@
+![Preview of BBClean](https://cloud.githubusercontent.com/assets/791189/6587656/7c5ba926-c787-11e4-9578-15f6f4b3399e.png)
+# BBClean - Blackboard Cleanup
+
+This Chrome browser plugin aims at removing some unnecessary content from Blackboard. It is currently optimised for the Blackboard of the TU Delft, but it can potentially be extended to support many more different Blackboard systems, and versions.
+
+### What the plugin does currently:
+
+- Moves the TU Delft (or other) logo next to the menu: reduces header by 42 pixels.
+- Removes customisation header bar, as this is now drag and drop by default anyway, and who does really apply colours to his Blackboard? No one right?
+- Moves ugly (!) vtbegenerated messages (the ones that look like they are from the 1992 internetz) to the right of the menu, so they are still visible but without the ugly style and less obtrusive.
+
+Feedback and extensions are welcome!
+
+### Installation instructions
+Until we publish the plugin to the Chrome Web Store the installation process is as follows:
+
+- [Download the zip file](https://github.com/hermanbanken/BBClean/archive/master.zip) containing the sources
+- Extract the zip somewhere
+- Go to the Chrome menu (three bars, right top) > More tools > Extensions (chrome://extensions/)
+- Check the 'developer mode' checkbox
+- Click the [*Load unpacked extension...* button](https://cloud.githubusercontent.com/assets/791189/6587148/d62da776-c780-11e4-9100-63f80c50f29e.png)
+- Select the folder (that is the extracted zip)
+- Surf to Blackboard and enjoy!
+
+### Screenshots
+
+| Before | After | 
+| ------ | ----- |
+| ![home-before](https://cloud.githubusercontent.com/assets/791189/6587146/d62d53a2-c780-11e4-9b1b-db8ec208a292.png) | ![home-after](https://cloud.githubusercontent.com/assets/791189/6587149/d62df988-c780-11e4-90e9-6b9595ee6f8f.png) |
+| ![bulletin-before](https://cloud.githubusercontent.com/assets/791189/6587150/d62e4dfc-c780-11e4-95b9-3e2e2cc511fb.png) | ![bulletin-after](https://cloud.githubusercontent.com/assets/791189/6587147/d62d9894-c780-11e4-9be9-ba202cd7aba8.png) | ![install](https://cloud.githubusercontent.com/assets/791189/6587148/d62da776-c780-11e4-9100-63f80c50f29e.png)
+
+### Disclaimer
+
+Please note that not all Blackboard features are tested with this plugin. It might break some fancy little hidden feature somewhere in the beast called Blackboard.
+
+I'm not affiliated with Blackboard, nor do I intend to infringe any copyright of Blackboard or the companies that own it. Using this plugin we solely display a Blackboard experience that we would like to be the official Blackboard experience. We do encourage Blackboard engineers to use our (visual) suggestions and incorporate them in their product. We wont sue you for improving Blackboard using these suggestions.
---
+++ clean.js
@@ -0,0 +1,82 @@
+function log(){ if(console) console.log.apply(console, arguments); };
+$.fn.fc = function(){
+	return $(this.get(0).contentDocument);
+}
+
+var contentFrame = $("#contentFrame").css("height", "+=42").fc(),
+		navFrame = $("#navFrame").css("height", "-=42").fc();
+
+navFrame.ready(function(){
+	/* Move logo */
+	var firstMenuItem = $("#appTabList tr td", navFrame).first();
+	var logo = navFrame.find("#topTabs .brandingImgWrap");
+	navFrame.find("#topTabs").css("height", "47px");
+	logo.wrap("<td id='logo'></td>").parent().insertBefore(firstMenuItem);
+
+	/* Make logo a _blank link */
+	logo.find("a").attr("href", "https://blackboard.tudelft.nl/webapps/portal/frameset.jsp?tab_tab_group_id=_9_1");
+	logo.find("a").attr("target", "_top");
+	if(window.devicePixelRatio > 1)
+		logo.find("img").attr("src", chrome.extension.getURL("tudelft_logo.png"));
+
+	/* Remove MyContent */
+	navFrame.find("[id='My Content']").remove();
+
+	/* Remove MyStudentInfo */
+	navFrame.find("#MyStudentInfo").remove();
+
+	/* Remove MyCampusLife, Add Collegerama */
+	navFrame.find("[id='My Campus Life']")
+		.attr("id","Collegerama").insertBefore(navFrame.find("#Organizations"))
+		.find("a").attr("href", "http://collegeramacolleges.tudelft.nl/online/faculteiten/").attr("target", "_blank")
+		.find("span:not(.hideoff)").text("Collegerama");
+});
+
+contentFrame.ready(function(){
+	/* Remove unnecessary bars */
+	contentFrame.find("#paneTabs, #actionbar").remove();
+	contentFrame.find(".locationPane").find("> .paneTabs").remove();
+	contentFrame.find("[id='module:_2914_1']").remove(); //Blackboard IM module
+	contentFrame.find("[id='module:_2674_1']").remove(); //Mobile learning module
+	contentFrame.find("#content > .container");
+	
+	/* Move ugly bulletin messages */
+	var texts = contentFrame.find(".vtbegenerated:not(.portlet *)");
+	if(texts.size()){
+		var h = "", a = texts.map(function(){ return this && $(this).html(); });
+		for(var i = 0; i < a.length; a++){
+			var c = a[i].replace(/\<script[\s\S]*\<\/script\>/ig, "");
+			if($(c).text().trim() != "")
+				h += c;
+		}
+
+		if(h){
+			var asDOM = $(h);
+			asDOM.removeAttr("style").find("[style]").removeAttr("style");
+			h = asDOM.html();
+			$("<td id='VBMessages'><div class='vbmessages'></div></td>").insertAfter(navFrame.find("#topTabs #Help"));
+			navFrame.find("#VBMessages .vbmessages").append(h);		
+		}
+	}
+	texts.remove();
+});
+
+jQuery(document).ready(function(){
+	log("Blackboard was succesfully cleaned by your favorite Blackboard cleanup plugin BBClean. Thanks for watching!");
+	log("You can fork and contribute at https://github.com/hermanbanken/BBClean");
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {
+                        hostEquals: 'blackboard.tudelft.nl'
+                    }
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
